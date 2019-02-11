023ac093fbb16783298b51503b35f5f892d1ae6a
Manuel Cabras <cabrasm@outlook.com>
2019-02-11 22:02:58 +0100

    implementation of graphs in main view. Not done yet

--- app/src/main/java/vergecurrency/vergewallet/helpers/GraphUtils.java
+++ app/src/main/java/vergecurrency/vergewallet/helpers/GraphUtils.java
@@ -1,6 +1,134 @@
 package vergecurrency.vergewallet.helpers;
 
-public class GraphUtils {
+import android.graphics.Color;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import vergecurrency.vergewallet.R;
+import vergecurrency.vergewallet.models.dataproc.GraphsDataReader;
+import vergecurrency.vergewallet.structs.GraphInfo;
+
+public final class GraphUtils {
+
+	public static void createChart(CombinedChart cc) {
+		GraphInfo gi = GraphsDataReader.readPriceStatistics();
+
+		List<Entry> volumeData = createEntryList(gi.getVolume_usd().entrySet());
+		List<Entry> priceData = createEntryList(gi.getPrice_usd().entrySet());
+
+		cc.getDescription().setEnabled(false);
+		cc.setBackgroundColor(Color.WHITE);
+		cc.setDrawGridBackground(false);
+		cc.setHighlightFullBarEnabled(false);
+
+
+		//to play with, seems nice
+		cc.setDrawBarShadow(false);
+
+		cc.setDrawOrder(new CombinedChart.DrawOrder[]{CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE});
+
+
+		Legend l = cc.getLegend();
+		l.setWordWrapEnabled(true);
+		l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+		l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+		l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+		l.setDrawInside(false);
+
+
+		YAxis rightAxis = cc.getAxisRight();
+		rightAxis.setDrawGridLines(false);
+		rightAxis.setAxisMinimum(0f);
+		rightAxis.setDrawLabels(false);
+
+		YAxis leftAxis = cc.getAxisLeft();
+		leftAxis.setDrawGridLines(false);
+		leftAxis.setAxisMinimum(0f);
+		leftAxis.setDrawLabels(false);
+
+
+		XAxis xAxis = cc.getXAxis();
+		xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+		xAxis.setAxisMinimum(0f);
+		xAxis.setGranularity(1f);
+		xAxis.setDrawGridLines(false);
+		xAxis.setDrawLabels(false);
+
+
+		//TODO : later
+		CombinedData data = new CombinedData();
+		data.setData(generateBarData(volumeData));
+		data.setData(generateLineData(priceData));
+
+		xAxis.setAxisMaximum(data.getXMax()+0.25f);
+
+		cc.setData(data);
+		cc.invalidate();
+
+
+	}
+
+	private static List<Entry> createEntryList(Set<Map.Entry<String, String>> set) {
+		List<Entry> entries = new ArrayList<>();
+
+		for (Map.Entry<String, String> e : set) {
+			entries.add(new Entry(Float.parseFloat(e.getKey()), Float.parseFloat(e.getValue())));
+		}
+
+		return entries;
+	}
+
+	private static BarData generateBarData(List<Entry> data) {
+		BarData barData = new BarData();
+		ArrayList<BarEntry> barEntry = new ArrayList<>();
+
+		for (Entry e :data) {
+			barEntry.add(0, new BarEntry(e.getX(),e.getY()));
+		}
+
+		BarDataSet set = new BarDataSet(barEntry, "Bars");
+		set.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+		barData.addDataSet(set);
+		barData.setBarWidth(0.45f);
+
+		return barData;
+	}
+
+
+	private static LineData generateLineData(List<Entry> data)  {
+		LineData lineData = new LineData();
+
+		LineDataSet set = new LineDataSet(data,"Line");
+		set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+		set.setDrawValues(false);
+		set.setDrawCircles(false);
+		set.setDrawFilled(true);
+		set.setDrawHorizontalHighlightIndicator(false);
+		set.setLineWidth(1.5f);
+		set.setHighlightLineWidth(1f);
+		set.setFillAlpha(1);
+		set.setHighLightColor(R.color.verge_colorPrimary);
+
+		set.setAxisDependency(YAxis.AxisDependency.LEFT);
+		lineData.addDataSet(set);
+
+		return lineData;
+	}
 }
--- app/src/main/java/vergecurrency/vergewallet/views/fragments/walletpageviews/FragmentChartsPageView.java
+++ app/src/main/java/vergecurrency/vergewallet/views/fragments/walletpageviews/FragmentChartsPageView.java
@@ -1,6 +1,5 @@
 package vergecurrency.vergewallet.views.fragments.walletpageviews;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,23 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.CombinedChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.CombinedData;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import vergecurrency.vergewallet.R;
-import vergecurrency.vergewallet.models.dataproc.GraphsDataReader;
-import vergecurrency.vergewallet.structs.GraphInfo;
-
-import static com.github.mikephil.charting.charts.CombinedChart.DrawOrder;
+import vergecurrency.vergewallet.helpers.GraphUtils;
 
 public class FragmentChartsPageView extends Fragment {
 
@@ -46,63 +31,9 @@ public class FragmentChartsPageView extends Fragment {
 		combinedChart = (CombinedChart) rootView.findViewById(R.id.charts_card_chartview);
 
 		//TODO : Move all this shit to GraphUtils. RIGHT AFTER YOU'RE DONE TESTING MANUEL
-		createChart(combinedChart);
+		GraphUtils.createChart(combinedChart);
 
 		return rootView;
-	}
-
-	void createChart(CombinedChart cc) {
-		GraphInfo gi = GraphsDataReader.readPriceStatistics();
-
-		List<Entry> volumeData = fillGraphData(gi.getVolume_usd().entrySet());
-		List<Entry> priceData = fillGraphData(gi.getPrice_usd().entrySet());
-
-		cc.getDescription().setEnabled(false);
-		cc.setBackgroundColor(Color.WHITE);
-		cc.setDrawGridBackground(false);
-		cc.setHighlightFullBarEnabled(false);
-
-		//to play with, seems nice
-		cc.setDrawBarShadow(false);
-
-		cc.setDrawOrder(new DrawOrder[]{DrawOrder.BAR, DrawOrder.LINE});
-
-		YAxis rightAxis = cc.getAxisRight();
-		rightAxis.setDrawGridLines(false);
-		rightAxis.setAxisMinimum(0f);
-
-		YAxis leftAxis = cc.getAxisLeft();
-		leftAxis.setDrawGridLines(false);
-		leftAxis.setAxisMinimum(0f);
-
-		XAxis xAxis = cc.getXAxis();
-		xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
-		xAxis.setAxisMinimum(0f);
-		xAxis.setGranularity(1f);
-		//TODO : later
-		//xAxis.setValueFormatter( new ValueFormatter() {}
-
-		CombinedData data = new CombinedData();
-
-
-	}
-
-	private List<Entry> fillGraphData(Set<Map.Entry<String, String>> set) {
-		List<Entry> entries = new ArrayList<>();
-
-		for (Map.Entry<String, String> e : set) {
-			entries.add(new Entry(Float.parseFloat(e.getKey()), Float.parseFloat(e.getValue())));
-		}
-
-		return entries;
-	}
-
-	private BarData generateBarData() {
-		return null;
-	}
-
-	private LineData generateLineData()  {
-		return null;
 	}
 
 
