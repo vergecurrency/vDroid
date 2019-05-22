ca3ef1eccb443cd336af7a82d2c84dfc3750779d
Luke Biller <biller.luke@gmail.com>
2019-05-21 22:14:29 -0400

    show paper key view

--- app/src/main/java/vergecurrency/vergewallet/view/ui/activity/settings/ShowPaperkeyActivity.java
+++ app/src/main/java/vergecurrency/vergewallet/view/ui/activity/settings/ShowPaperkeyActivity.java
@@ -18,12 +18,17 @@ public class ShowPaperkeyActivity extends AppCompatActivity {
 	private TextView seedTextView;
 	private ImageView closeButton;
 
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_paperkey_shown);
 		mViewModel = ViewModelProviders.of(this).get(ShowPaperkeyViewModel.class);
+		initComponents();
 	}
 
+	//TODO : require passphrase before showing seed, create function for toggling between
+	// qr and text views
 
 	private void initComponents() {
 		seedTextView = findViewById(R.id.paperkey_shown_pk);
@@ -31,6 +36,12 @@ public class ShowPaperkeyActivity extends AppCompatActivity {
 		closeButton = findViewById(R.id.paperkey_shown_close_button);
 		closeButton.setOnClickListener(onCloseClickListener());
 	}
+
+
+
+
+
+
 
 	View.OnClickListener onCloseClickListener() {
 		return v -> {
