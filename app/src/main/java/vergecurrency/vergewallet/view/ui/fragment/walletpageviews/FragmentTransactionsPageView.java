7d1061cedc33a0aeb31e22d7b6da989525c88b77
sevco <severin.putzi@gmail.com>
2019-07-02 22:22:14 +0200

    styles

--- app/src/main/java/vergecurrency/vergewallet/view/ui/fragment/walletpageviews/FragmentTransactionsPageView.java
+++ app/src/main/java/vergecurrency/vergewallet/view/ui/fragment/walletpageviews/FragmentTransactionsPageView.java
@@ -33,7 +33,6 @@ public class FragmentTransactionsPageView extends Fragment {
 			rootView = inflater.inflate(R.layout.fragment_pageview_transactions, container, false);
 			ListView transactionList = rootView.findViewById(R.id.transactions_listview);
 			transactionList.setAdapter(new TransactionsAdapter(this.getContext(), mViewModel.getTransactionsList(), false));
-			transactionList.setDivider(null);
 		}
 
 		return rootView;
