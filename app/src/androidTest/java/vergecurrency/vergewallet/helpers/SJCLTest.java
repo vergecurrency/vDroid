3fbf8de474b50f1a7a3187b8fb41aef1a8d7f95d
2lazy2debug <cabrasm@outlook.com>
2019-10-09 00:20:16 +0200

    committing before disaster happens

--- app/src/androidTest/java/vergecurrency/vergewallet/helpers/SJCLTest.java
+++ app/src/androidTest/java/vergecurrency/vergewallet/helpers/SJCLTest.java
@@ -3,16 +3,29 @@ package vergecurrency.vergewallet.helpers;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class SJCLTest {
+	private SJCL sjcl;
 
+	public SJCLTest() {
+		sjcl = new SJCL(InstrumentationRegistry.getInstrumentation().getTargetContext());
+		sjcl.init();
+
+	}
 
 	@Test
 	public void base64ToBits() {
-		SJCL sjcl = new SJCL(InstrumentationRegistry.getInstrumentation().getTargetContext());
-
 		String result = sjcl.base64ToBits("dGVzdA==");
-		assertTrue(result.equals("1.952805748E9")); // well... kind of. To be corrected.
+		assertEquals("1952805748", result);
+	}
+
+	@Test
+	public void encryptAndDecrypt() {
+		String encoded = sjcl.encrypt("Happy", "Friday", new int[] {2048,256});
+		//String decoded = sjcl.decrypt("Happy", encoded);
+
+		assertEquals(true, encoded != null);
 	}
 }
--- app/src/main/java/vergecurrency/vergewallet/helpers/SJCL.java
+++ app/src/main/java/vergecurrency/vergewallet/helpers/SJCL.java
@@ -2,90 +2,72 @@ package vergecurrency.vergewallet.helpers;
 
 import android.content.Context;
 
-import org.mozilla.javascript.NativeArray;
-import org.mozilla.javascript.NativeObject;
+import com.eclipsesource.v8.V8;
+import com.eclipsesource.v8.V8Array;
+import com.eclipsesource.v8.V8Object;
 
 import java.io.InputStream;
-
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 import vergecurrency.vergewallet.Constants;
 import vergecurrency.vergewallet.helpers.utils.FileUtils;
 
 public class SJCL {
-
-	private ScriptEngine engine;
-	private org.mozilla.javascript.Context jsContext;
+	private Context c;
+	private V8 runtime;
+	private V8Object sjcl;
 
 	public SJCL(Context c) {
-
-		init(c);
+		this.c = c;
 	}
 
-	public void init(Context c) {
-		ScriptEngineManager manager = new ScriptEngineManager();
-		engine = manager.getEngineByName("rhino");
+	public void init() {
 		try {
-			InputStream is = c.getAssets().open(Constants.SJCL_FILE_PATH);
-			String sjcl = FileUtils.convertStreamToString(is);
 
-			// read script file
-			engine.eval(sjcl);
+			//get the script
+			InputStream is = c.getAssets().open(Constants.SJCL_FILE_PATH);
+			String sjclFile = FileUtils.convertStreamToString(is);
+
+			//init V8 Runtime
+			runtime = V8.createV8Runtime();
+			runtime.executeVoidScript(sjclFile);
+
+			sjcl = runtime.getObject("sjcl");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println("Can't do something..................................................");
 		}
 	}
 
-	public String encrypt(Object password, String plainText, String[] params) {
-		try {
-			Invocable invocable = (Invocable) engine;
+	public String encrypt(String password, String message, int[] params) {
 
-			return invocable.invokeFunction("sjcl.encrypt", password, plainText, params).toString();
-		} catch (ScriptException | NoSuchMethodException e) {
-			return null;
-		}
+		V8Object count = new V8Object(runtime).add("count", params[0]);
+		V8Object ks = new V8Object(runtime).add("ks",params[1]);
+
+		V8Array paramsArray = new V8Array(runtime).push(count).push(ks);
+
+		Object result = sjcl.executeJSFunction("encrypt",password,message,paramsArray);
+
+		count.release();
+		ks.release();
+		paramsArray.release();
+
+
+		return result.toString();
+
 	}
 
-	public String decrypt(Object password, String plainText, String[] params) {
-		try {
-			Invocable invocable = (Invocable) engine;
-			return invocable.invokeFunction("sjcl.decrypt", password, plainText, params).toString();
-		} catch (ScriptException | NoSuchMethodException e) {
-			return null;
-		}
+	public String decrypt(String password, String message) {
+	return "";
 	}
 
-	/**
-	 * This method might look confusing. So here a detailed explanation.
-	 * The SJCL Library has nested objects, sorry if it's not the correct phrasing, I'm no JS expert.
-	 * So, while in JS (and in Swift too) you could call the "toBits" method by just typing "sjcl.codec.base64.toBits",
-	 * well, you can't in Java because fuck doing things easily (thanks Mozilla tho). So, we have to
-	 * use the ScriptEngine to get the SJCL object as a NativeObject, which is basically a key-value pair
-	 * that will have as a key the object name and as values the sub-objects/methods.
-	 * So I had to cast the returned objects as NativeObjects and retrieve the correct keys according to the file structure.
-	 *
-	 * The result will come as a native array whereas the 0 index contains the result, which is turned to a String.
-	 *
-	 * @param encryptingKey the key in a string form
-	 * @return the string represantation of a bit array.
-	 */
 	public String base64ToBits(String encryptingKey) {
-		try {
-			Invocable invocable = (Invocable) engine;
-			Object o = ((NativeObject) ((NativeObject) engine.get("sjcl")).get("codec")).get("base64");
-			NativeArray o2 = (NativeArray) invocable.invokeMethod(o, "toBits", encryptingKey);
+		V8Object base64 = sjcl.getObject("codec").getObject("base64");
 
-			//THIS IS STILL NOT WORKING AS EXPECTED.
-			String result = o2.get(0).toString();
-			return result;
+		Object result = base64.executeJSFunction("toBits",encryptingKey);
 
-		} catch (ScriptException | NoSuchMethodException e) {
-			e.printStackTrace();
-			return null;
-		}
+		base64.release();
+
+		return result.toString();
 	}
 }
