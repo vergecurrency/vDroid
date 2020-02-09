8a611dfbd40e5eb8dad55052b27ce73c6b62cccd
Corey Kreiniker <corey.kreiniker@gmail.com>
2020-02-09 20:04:49 +0100

    Unified pin views (#49)
    Co-authored-by: 2lazy2debug <cabrasm@outlook.com>

--- app/src/main/kotlin/vergecurrency/vergewallet/view/ui/activity/firstlaunch/FirstLaunchActivity.kt
+++ app/src/main/kotlin/vergecurrency/vergewallet/view/ui/activity/firstlaunch/FirstLaunchActivity.kt
@@ -2,12 +2,15 @@ package vergecurrency.vergewallet.view.ui.activity.firstlaunch
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spanned
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.text.HtmlCompat
-import vergecurrency.vergewallet.R
+
 import vergecurrency.vergewallet.view.base.BaseActivity
+import vergecurrency.vergewallet.R
 
 class FirstLaunchActivity : BaseActivity() {
 
@@ -24,8 +27,6 @@ class FirstLaunchActivity : BaseActivity() {
         //create "Create new wallet" button and its and listener
         bottomButton = findViewById(R.id.button_create_wallet)
         bottomButton.setText(HtmlCompat.fromHtml(resources.getString(R.string.first_launch_new_wallet_button), HtmlCompat.FROM_HTML_MODE_LEGACY), TextView.BufferType.SPANNABLE)
-
-
         bottomButton.setOnClickListener(newWalletOnClickListener())
 
         //create "Restore wallet"  button and its and listener.
@@ -44,7 +45,7 @@ class FirstLaunchActivity : BaseActivity() {
         }
     }
 
-    private fun restoreWalletOnClickListener(): View.OnClickListener {
+    private fun restoreWalletOnClickListener():View.OnClickListener {
         return View.OnClickListener {
             //HERE TOO YOU ASSHOLE OF A DEV
         }
@@ -52,3 +53,4 @@ class FirstLaunchActivity : BaseActivity() {
 
 
 }
+
