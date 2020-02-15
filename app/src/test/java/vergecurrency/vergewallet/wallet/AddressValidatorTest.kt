d9afe9c588af22667679255d59f65dec58327a5b
2lazy2debug <cabrasm@outlook.com>
2020-02-15 22:50:23 +0100

    added test cases to AddressValidatorTest

--- app/src/test/java/vergecurrency/vergewallet/wallet/AddressValidatorTest.kt
+++ app/src/test/java/vergecurrency/vergewallet/wallet/AddressValidatorTest.kt
@@ -34,5 +34,49 @@ internal class AddressValidatorTest {
             assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
             assertTrue(amount == null)
         }
+
+        validator.validate("verge://DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == null)
+        }
+
+        validator.validate("verge:DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == null)
+        }
     }
+
+    @Test
+    fun testValidationValidAddressWithAmount() {
+
+        val validator = AddressValidator()
+
+        validator.validate("DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu?amount=1000.0") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == 1000.0f)
+        }
+
+        validator.validate("DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu?amount=1000.43522") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == 1000.43522f)
+        }
+
+        validator.validate("verge://DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu?amount=1000.43522") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == 1000.43522f)
+        }
+
+        validator.validate("verge:DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu?amount=10330.43522") {valid, address, amount ->
+            assertTrue(valid)
+            assertTrue(address == "DCys4K9buSLAgd4jG9qqZn3vB9CdXJLMJu")
+            assertTrue(amount == 10330.43522f)
+        }
+    }
+
+
 }
