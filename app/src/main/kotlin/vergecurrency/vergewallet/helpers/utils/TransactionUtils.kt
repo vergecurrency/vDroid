f2538f72caea59667366aafb0a29b8faa7f86146
PuuTheBear <>
2020-02-29 15:28:39 +0100

    Updated tx look

--- app/src/main/kotlin/vergecurrency/vergewallet/helpers/utils/TransactionUtils.kt
+++ app/src/main/kotlin/vergecurrency/vergewallet/helpers/utils/TransactionUtils.kt
@@ -1,17 +1,15 @@
 package vergecurrency.vergewallet.helpers.utils
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.text.format.DateFormat
-import java.text.SimpleDateFormat
 import java.util.*
 
 object TransactionUtils {
     const val EXTRA_TRANSACTION = "transaction"
 
-    fun toFormattedDate(milliseconds: Long, appContext: Context): String {
+    fun toFormattedDate(milliseconds: Long, context: Context): String {
         val date = Date(milliseconds * 1000)
-        return String.format("%s %s", DateFormat.getMediumDateFormat(appContext).format(date), DateFormat.getTimeFormat(appContext).format(date));
+        return String.format("%s %s", DateFormat.getMediumDateFormat(context).format(date), DateFormat.getTimeFormat(context).format(date));
     }
 }
 
